c80eab3b74d1582c44b0ba30f9c9daa59f8f3cd6
Erlang Parasu <erlangparasu@gmail.com>
2022-02-02 19:47:59 +0800

    parse namespace and folder path

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 //
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { TextEncoder } from 'util';
+import { TextDecoder, TextEncoder } from 'util';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -24,16 +24,29 @@ export function activate(context: vscode.ExtensionContext) {
         console.log('scheme', a.scheme);
 
         let uri = vscode.Uri.parse(a.path);
-        // let c = await vscode.workspace.fs.readFile(uri);
+        let dataUint8Arr = await vscode.workspace.fs.readFile(uri);
+        let contextContent = new TextDecoder("utf-8").decode(dataUint8Arr);
+        // console.log('contextContent:', contextContent);
+
+        let parsedNamespace = parseNameSpaceFromFileContent(contextContent);
+        console.log('parsedNamespace:', parsedNamespace);
 
         // console.log('b:', b);
         // console.log('c:', c);
         // console.log('d:', d);
 
+        let folderPath = parseFolderFromPath(a.path);
+        console.log('folderPath:', folderPath);
+
         vscode.window.showInputBox({ placeHolder: "Please enter a file name", }).then((fileName) => {
-            let data = generateFileData(fileName);
+            let data = generateFileData(fileName, parsedNamespace, folderPath);
             if (null !== data) {
-                return vscode.workspace.fs.writeFile(vscode.Uri.parse(a.path + '/' + data[0] + ''), new TextEncoder().encode(data[1]));
+                if (folderPath !== null) {
+                    return vscode.workspace.fs.writeFile(
+                        vscode.Uri.parse(folderPath + '' + data[0] + ''),
+                        new TextEncoder().encode(data[1])
+                    );
+                }
             }
         });
 
@@ -45,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-function generateFileData(fileName: string | undefined) {
+function generateFileData(fileName: string | undefined, parsedNamespace: string | null, folderPath: string | null) {
     if (undefined === fileName) {
         return null;
     }
@@ -70,10 +83,18 @@ function generateFileData(fileName: string | undefined) {
         return alp.split('').indexOf(d) > -1;
     }).join('');
 
+    parsedNamespace = (parsedNamespace ?? '').split('').filter(function (d, i, arr) {
+        let alp = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ\\';
+        alp = alp + alp.toLowerCase();
+        return alp.split('').indexOf(d) > -1;
+    }).join('');
+
     let raw = getClassStub();
     raw = raw.replace('{{className}}', className);
 
-    // TODO {{namespace}}
+    if (parsedNamespace !== '') {
+        raw = raw.replace('{{namespace}}', parsedNamespace);
+    }
 
     let data = [];
     data[0] = className + '.php';
@@ -114,3 +135,42 @@ class {{className}}
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+function parseNameSpaceFromFileContent(content: string) {
+    // "namespace App\Http;"
+    let posStart = content.indexOf('namespace ');
+    if (posStart >= 0) {
+        let posEnd = posStart + content.substring(posStart).indexOf(';');
+        let foundStr = content.substring(posStart, posEnd);
+
+        // "namespace App\Services\Socialite"
+        let str = foundStr.replace('namespace ', '').replace(';', '');
+        // "App\Services\Socialite"
+
+        return str;
+    }
+
+    return null;
+}
+
+function parseFolderFromPath(path: string) {
+    // "/usr/local/var/www/abc/app/Services/Socialite/SocialiteServerService.php"
+    if (path.indexOf('/') >= 0) {
+        // linux
+        let strFilename;
+        let segments = path.split('/');
+        if (segments.length > 0) {
+            let lastSegment = segments[segments.length - 1];
+            if (lastSegment.toLowerCase().indexOf('.php') >= 0) {
+                strFilename = segments.pop();
+            }
+        }
+        if (strFilename) {
+            let resultFolderPath = path.replace(strFilename, '');
+            // "/usr/local/var/www/abc/app/Services/Socialite/"
+            return resultFolderPath;
+        }
+    }
+
+    return null;
+}
